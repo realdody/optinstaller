8972a98a74bc4fe4c157785519f44627ba52d6f7
yeahimman <dodobozicek@gmail.com>
2026-01-22 22:45:25 +0100

    fix: why doesn't coderabbit just say everything in one review

--- Optinstaller/ViewLocator.cs
+++ Optinstaller/ViewLocator.cs
@@ -6,9 +6,6 @@ using Optinstaller.ViewModels;
 
 namespace Optinstaller;
 
-/// <summary>
-/// Given a view model, returns the corresponding view if possible.
-/// </summary>
 [RequiresUnreferencedCode(
     "Default implementation of ViewLocator involves reflection which may be trimmed away.",
     Url = "https://docs.avaloniaui.net/docs/concepts/view-locator")]
